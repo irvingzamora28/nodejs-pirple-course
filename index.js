fc939e77bef4280b789a58ff7490c9d17955a282
Irving Zamora <irvingzamora28@gmail.com>
2021-06-02 10:45:45 -0600

    Parsed request path

--- index.js
+++ index.js
@@ -3,11 +3,29 @@
  */
 
 // Dependencies
-import http from 'http';
+import http from 'http'
+import url from 'url'
+
+const host = 'http://localhost'
+const port = 3000;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-    res.end('Hello World\n')
+    // Get URL and parse it
+    const parsedURL = new URL(`${host}:${port}${req.url}`);
+
+
+    // Get the path
+    const path = parsedURL.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+    
+    // Send the response
+    res.end(trimmedPath)
+    // res.end('Hello World\n')
+    
+
+    // Log the request path
+
 })
 
 // Start the server, and have it listen on port 3000
