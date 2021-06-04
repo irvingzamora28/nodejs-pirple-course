f0048a78770ddf79f16f7fd0eb64ea440dc86139
Irving Zamora <irvingzamora28@gmail.com>
2021-06-03 23:01:26 -0600

    Parsed payload

--- index.js
+++ index.js
@@ -4,7 +4,8 @@
 
 // Dependencies
 import http from 'http'
-import url from 'url'
+import querystring from 'querystring'
+import {StringDecoder} from 'string_decoder'
 
 const host = 'http://localhost'
 const port = 3000;
@@ -18,13 +19,38 @@ const server = http.createServer((req, res) => {
     // Get the path
     const path = parsedURL.pathname;
     const trimmedPath = path.replace(/^\/+|\/+$/g, '')
-    
-    // Send the response
-    res.end(trimmedPath)
-    // res.end('Hello World\n')
-    
 
-    // Log the request path
+    // Get query string as an object
+    var queryStringObject = querystring.parse(parsedURL.search.replace(/^.*\?/, ''))
+
+    // Get HTTP method 
+    const method = req.method.toUpperCase()
+
+    // Get headers as an object
+    const headers = req.headers
+
+    // Get payload, if any
+    const decoder = new StringDecoder('utf-8')
+    let buffer = ''
+    req.on('data',(data) => {
+        buffer += decoder.write(data)
+    })
+
+    req.on('end', () => {
+        buffer += decoder.end()
+
+        // Send the response
+        res.end(`Hello World`)
+        
+        // Log the request path
+        console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters \n`)
+        console.log(queryStringObject);
+        console.log(headers);
+        console.log('Request receive with this payload:');
+        console.log(buffer);
+    })
+
+
 
 })
 
