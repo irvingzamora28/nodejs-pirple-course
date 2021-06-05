bbcb2daabc6b5c2ca1c8188330d4abf49dcbbbd3
Irving Zamora <irvingzamora28@gmail.com>
2021-06-04 23:41:13 -0600

    Routing requests

--- index.js
+++ index.js
@@ -39,18 +39,45 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
         buffer += decoder.end()
 
-        // Send the response
-        res.end(`Hello World`)
+        // Choose the handler this request shoudl go to. If one is not found, use the notFound handler
+        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+
+        // Construct data object to send to the handler
+        let data = {
+            'trimmedPath' : trimmedPath,
+            'queryStringObject' : queryStringObject,
+            'method' : method,
+            'headers' : headers,
+            'payload' : buffer
+        }
+
+        // Route the request to the habdler specified in the router
+        chosenHandler(data, (statusCode, payload) => {
+            // Use the status code called back by the handler or default 200
+            statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+
+            // Use the payload called back by the handler, or default empty object
+            payload = typeof(payload) == 'object' ? payload : {}
+
+            // Convert the payload to a string
+            let payloadString = JSON.stringify(payload)
+
+            // Return response
+            res.writeHead(statusCode)
+            res.end(payloadString)
+
+
+            // Log the request path
+            console.log('Returning this response: ', statusCode, payloadString);
+            console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters \n`)
+            console.log(queryStringObject);
+            console.log(headers);
+            console.log('Request receive with this payload:');
+            console.log(buffer);
+        })
+
         
-        // Log the request path
-        console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters \n`)
-        console.log(queryStringObject);
-        console.log(headers);
-        console.log('Request receive with this payload:');
-        console.log(buffer);
     })
-
-
 
 })
 
@@ -58,3 +85,19 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('The server is listening on port 3000');
 })
+
+// Define handlers
+const handlers = {}
+handlers.sample = (data, callback) => {
+    // Callback a http status code, and a payload object
+    callback(406, {'name': 'Sample handler'})
+}
+
+handlers.notFound = (data, callback) => {
+    callback(404)
+}
+
+// Define a request router
+const router = {
+    'sample' : handlers.sample
+}
