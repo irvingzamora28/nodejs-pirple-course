d6975d102310109772907b95d28de48d84c50954
Irving Zamora <irvingzamora28@gmail.com>
2021-06-05 00:26:03 -0600

    Set content type application/json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 // Dependencies
 import http from 'http'
 import querystring from 'querystring'
-import {StringDecoder} from 'string_decoder'
+import { StringDecoder } from 'string_decoder'
 
 const host = 'http://localhost'
 const port = 3000;
@@ -32,7 +32,7 @@ const server = http.createServer((req, res) => {
     // Get payload, if any
     const decoder = new StringDecoder('utf-8')
     let buffer = ''
-    req.on('data',(data) => {
+    req.on('data', (data) => {
         buffer += decoder.write(data)
     })
 
@@ -40,29 +40,30 @@ const server = http.createServer((req, res) => {
         buffer += decoder.end()
 
         // Choose the handler this request shoudl go to. If one is not found, use the notFound handler
-        let chosenHandler = typeof(router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
+        let chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handlers.notFound;
 
         // Construct data object to send to the handler
         let data = {
-            'trimmedPath' : trimmedPath,
-            'queryStringObject' : queryStringObject,
-            'method' : method,
-            'headers' : headers,
-            'payload' : buffer
+            'trimmedPath': trimmedPath,
+            'queryStringObject': queryStringObject,
+            'method': method,
+            'headers': headers,
+            'payload': buffer
         }
 
         // Route the request to the habdler specified in the router
         chosenHandler(data, (statusCode, payload) => {
             // Use the status code called back by the handler or default 200
-            statusCode = typeof(statusCode) == 'number' ? statusCode : 200
+            statusCode = typeof (statusCode) == 'number' ? statusCode : 200
 
             // Use the payload called back by the handler, or default empty object
-            payload = typeof(payload) == 'object' ? payload : {}
+            payload = typeof (payload) == 'object' ? payload : {}
 
             // Convert the payload to a string
             let payloadString = JSON.stringify(payload)
 
             // Return response
+            res.setHeader('Content-Type', 'application/json')
             res.writeHead(statusCode)
             res.end(payloadString)
 
@@ -76,7 +77,7 @@ const server = http.createServer((req, res) => {
             console.log(buffer);
         })
 
-        
+
     })
 
 })
@@ -90,7 +91,7 @@ server.listen(3000, () => {
 const handlers = {}
 handlers.sample = (data, callback) => {
     // Callback a http status code, and a payload object
-    callback(406, {'name': 'Sample handler'})
+    callback(406, { 'name': 'Sample handler' })
 }
 
 handlers.notFound = (data, callback) => {
@@ -99,5 +100,5 @@ handlers.notFound = (data, callback) => {
 
 // Define a request router
 const router = {
-    'sample' : handlers.sample
+    'sample': handlers.sample
 }
