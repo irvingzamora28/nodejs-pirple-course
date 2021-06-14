1b3b9c4e3fea192e36f82953ddd98e2eaa37ae7a
Irving Zamora <irvingzamora28@gmail.com>
2021-06-13 23:47:22 -0600

    Added CRUD for checks

--- index.js
+++ index.js
@@ -131,5 +131,6 @@ let unifiedServer = (req, res) => {
 const router = {
     'ping': handlers.ping,
     'users': handlers.users,
-    'tokens': handlers.tokens
+    'tokens': handlers.tokens,
+    'checks': handlers.checks
 }
